2dfe0d262ac975d6b1283d0d2e284d49768ac555
Brandon Hudgeons <brandon@atsoft.at>
2015-06-08 18:24:47 -0500

    updating start script

--- minecraft-server-start-nogui.sh
+++ minecraft-server-start-nogui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/forsawcraften 
-screen -c /dev/null -dmS minecraft java -classpath "/home/ec2-user/forsawcraften/lib/scala-library-2.10.2.jar" -XX:MaxPermSize=128M -Xmx1024M -Xms1024M -jar spigot.jar nogui 
+screen -c /dev/null -dmS minecraft java -classpath "/home/ec2-user/forsawcraften/lib/scala-library.jar" -XX:MaxPermSize=128M -Xmx1024M -Xms1024M -jar spigot.jar nogui 
