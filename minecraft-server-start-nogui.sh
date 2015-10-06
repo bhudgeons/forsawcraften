714ffc15992d102982fdcda83b7eb679471266c9
ATsoft Buildsystem <builds@atsoft.at>
2015-10-06 20:48:35 +0000

    intermediate world save

--- minecraft-server-start-nogui.sh
+++ minecraft-server-start-nogui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/forsawcraften 
-screen -c /dev/null -dmS minecraft java -XX:MaxPermSize=128M -Xmx1024M -Xms1024M -jar spigot.jar nogui 
+screen -c /dev/null -dmS minecraft java -XX:MaxPermSize=128M -Xmx1024M -Xms1024M -cp 'spigot.jar:lib/*' org.bukkit.craftbukkit.Main nogui 
